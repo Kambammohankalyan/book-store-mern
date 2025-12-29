6070c3d8a3e6724e19026e85f2f329cb5e3d0b36
Kambammohankalyan <kambammohankalyan@gmail.com>
2025-12-29 17:38:10 +0530

    fix root route

--- backend/index.js
+++ backend/index.js
@@ -24,14 +24,16 @@ app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
+app.get("/", (req, res) => {
+  res.send("Book Store Server is running!");
+});
+
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
+  console.log("Mongodb connect successfully!");
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main().catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
